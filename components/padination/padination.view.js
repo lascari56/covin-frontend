d0c9e4445d5a51d62b63411fee1b3aa2e0c2d147
lascari <lascari565758@gmail.com>
2022-06-11 23:13:23 +0300

    push

--- components/padination/padination.view.js
+++ components/padination/padination.view.js
@@ -6,7 +6,7 @@ import * as S from "./padination.styled"
 
 const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
-export default function PadinationView({children, total, pageCount, onChange}) {
+export default function PadinationView({children, value, total, pageCount, onChange}) {
   return (
     <S.Container>
       <ReactPaginate
