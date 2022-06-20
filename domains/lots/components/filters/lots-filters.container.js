560d948d46eb956b467e62e83de92a3d32675e8f
lascari <lascari565758@gmail.com>
2022-06-20 17:25:40 +0300

    fixes

--- domains/lots/components/filters/lots-filters.container.js
+++ domains/lots/components/filters/lots-filters.container.js
@@ -203,9 +203,9 @@ export default function LotsFiltersContainer({data, onFilter, ...props}) {
       location: validateAll(values.location, filters.location),
       document: validateAll(values.document, filters.document),
       site: validateAll(values.site, filters.site),
-      year: filters.year,
-      odometr: filters.odometr,
-      cost_repair: filters.odometr,
+      year: values.year,
+      odometr: values.odometr,
+      cost_repair: values.odometr,
     }
 
     console.log(res)
