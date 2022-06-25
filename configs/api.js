deb9acdd0c39ded3792aa2e8be476355841101d7
lascari <lascari565758@gmail.com>
2022-06-26 00:25:13 +0300

    push

--- configs/api.js
+++ configs/api.js
@@ -1,4 +1,4 @@
-// const API = 'https://covin-dev.herokuapp.com';
-const API = 'http://localhost:3030';
+const API = 'https://covin-dev.herokuapp.com';
+// const API = 'http://localhost:3030';
 
 export {API};
