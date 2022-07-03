742197c2ad459927843110ff6f88b8fb0aa9bfb2
lascari <lascari565758@gmail.com>
2022-07-03 15:18:30 +0300

    wefef

--- domains/settings/components/personal-info/setting-personal-info.container.js
+++ domains/settings/components/personal-info/setting-personal-info.container.js
@@ -4,6 +4,8 @@ import SettingPersonalInfoView from "./setting-personal-info.view"
 
 import {useFormik} from 'formik';
 
+import * as yup from 'yup';
+
 const validationSchema = yup.object().shape({
   email: yup.string().email().required(),
   password: yup.string().min(6).required(),
