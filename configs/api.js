0215d9442652aab4470b7cb3d2b3ec767d28603b
lascari <lascari565758@gmail.com>
2022-07-12 00:00:37 +0300

    fixed

--- configs/api.js
+++ configs/api.js
@@ -1,5 +1,5 @@
-// const API = 'https://covin-dev.herokuapp.com';
-const API = 'http://localhost:3030';
+const API = 'https://covin-dev.herokuapp.com';
+// const API = 'http://localhost:3030';
 const STORAGE_URL = 'https://unocreative.ams3.cdn.digitaloceanspaces.com';
 
 export {API, STORAGE_URL};
