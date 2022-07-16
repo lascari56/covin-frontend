dba1ad454084e9236378bde8faf9f20a6049b02f
lascari <lascari565758@gmail.com>
2022-07-16 11:39:31 +0300

    fixed

--- components/pagination/padination.view.js
+++ components/pagination/padination.view.js
@@ -11,11 +11,11 @@ export default function PadinationView({className, children, localValue, value, 
     <S.Container className={className}>
       <S.Content>
         <S.Item disabled={value === 0} onClick={onPrev}>
-          <Icon name="arr-left" />
+          <Icon name="arr-left" size={12} />
         </S.Item>
 
         <S.Item disabled={value === total} onClick={onNext}>
-          <Icon name="arr-right" />
+          <Icon name="arr-right" size={12} />
         </S.Item>
 
         <S.Inner size="small" theme="white" value={localValue} onChange={onChange} onBlur={onBlur} onSubmit={onBlur} onKeyPress={onKeyUp} />
